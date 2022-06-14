3b82c9f3eea281b6cd62055e0fbb2ae2af118113
Jfontaine <jfontaine19@gmail.com>
2022-06-14 16:20:03 -0400

    Added FormControl from Material ui

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { FormControl, InputLabel, Input } from "@mui/material";
 
 function TodoList() {
   const [input, setInput] = useState("");
@@ -11,19 +13,32 @@ function TodoList() {
   const addTodo = (e) => {
     // this will fire when we click the button
     e.preventDefault();
-
     // this will fire when we hit the button
     console.log("im working");
     // We use a spread to that we keep the original contents of the todolist
     setTodos([...todos, input]);
+    // clear the input after the submit
+    setInput("");
   };
 
   return (
     <div classlist="todoList">
       <form>
         <h1>What's the Plan?</h1>
-        <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <button onClick={addTodo}>Add Todo</button>
+
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        </FormControl>
+
+        <Button
+          type="submit"
+          varient="outlined"
+          onClick={addTodo}
+          disabled={!input}
+        >
+          Add Todo
+        </Button>
       </form>
 
       <ul>
