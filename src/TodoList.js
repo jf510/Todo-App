ec4a8ce4d5714040b9e096865de574db30ca3737
Jfontaine <jfontaine19@gmail.com>
2022-06-14 15:44:47 -0400

    created state for the input. Added the form. added the addTodo function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
-import NewTodoForm from "./NewTodoForm";
 
 function TodoList() {
+  const [input, setInput] = useState("");
   const [todos, setTodos] = useState([
     "Take dogs for a walk",
     "Feed the cat",
     "Cook Dinner",
   ]);
-    
 
+  const addTodo = (e) => {
+    // this will fire when we click the button
+    e.preventDefault();
+
+    // this will fire when we hit the button
+    console.log("im working");
+    // We use a spread to that we keep the original contents of the todolist
+    setTodos([...todos, input]);
+  };
 
   return (
     <div classlist="todoList">
-      <NewTodoForm />
+      <form>
+        <h1>What's the Plan?</h1>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+      </form>
 
       <ul>
         {todos.map((todo) => (
